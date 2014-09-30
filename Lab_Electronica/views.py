e05e0254d2cf6e676ef6e4dfb69e883db699325b
acb065 <acb0654@gmail.com>
2014-09-30 11:48:24 -0600

    Revert "Revert "Initial commit""
    This reverts commit ddc88062b6e0d8f4cfe52e8ca7f98413d6dd09ac.

---
+++ Lab_Electronica/views.py
@@ -0,0 +1,46 @@
+from rest_framework.permissions import IsAuthenticated
+from Lab_Electronica.models import Student, DetailCart, DetailHistory, Component, Category
+from rest_framework import viewsets
+from Lab_Electronica import serializers
+
+
+# Create your views here.
+class StudentViewSet(viewsets.ModelViewSet):
+    """
+     API endpoint tha allows groups to be viewed or edited.
+    """
+    permission_classes = (IsAuthenticated,)
+    queryset = Student.objects.all()
+    serializer_class = serializers.StudentSerializer
+
+
+class DetailCartViewSet(viewsets.ModelViewSet):
+    """
+     API endpoint tha allows groups to be viewed or edited.
+    """
+    queryset = DetailCart.objects.all()
+    serializer_class = serializers.DetailCartSerializer
+
+
+class DetailHistoryViewSet(viewsets.ModelViewSet):
+    """
+     API endpoint tha allows groups to be viewed or edited.
+    """
+    queryset = DetailHistory.objects.all()
+    serializer_class = serializers.DetailHistorySerializer
+
+
+class ComponentViewSet(viewsets.ModelViewSet):
+    """
+     API endpoint tha allows groups to be viewed or edited.
+    """
+    queryset = Component.objects.all()
+    serializer_class = serializers.ComponentSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+     API endpoint tha allows groups to be viewed or edited.
+    """
+    queryset = Category.objects.all()
+    serializer_class = serializers.CategorySerializer
