3657d4487db6a686da74d7168713f334a769af05
Armando Colomo Baray <acb0654@gmail.com>
2015-02-14 18:46:36 -0700

    added django_filters

--- LabsAPI/settings.py
+++ LabsAPI/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'django_filters',
     'rest_framework',
     'Lab_Electronica',
     'BetaTesters',
