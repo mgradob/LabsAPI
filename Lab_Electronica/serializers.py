91bdf9174f835ed6142387da29ccc5cf54d038cd
Alex Rodriguez <ardzoht@gmail.com>
2015-08-24 14:35:50 -0600

    specified date_out field for automatic save on database

--- Lab_Electronica/serializers.py
+++ Lab_Electronica/serializers.py
@@ -2,14 +2,16 @@ __author__ = 'miguel'
 
 from rest_framework import serializers
 from Lab_Electronica.models import DetailCart, DetailHistory, Component, Category
+from datetime import datetime
+from django.utils.timezone import activate, now, localtime
 
 class DetailCartSerializer(serializers.ModelSerializer):
     #detail_cart = serializers.HyperlinkedRelatedField(many=True, view_name='DetailCart-detail', queryset=DetailCart.objects.all())
     #id_cart = serializers.IntegerField()
     def create(self, validated_data):
-	cart_data = validated_data.pop('owner')
-	cart = DetailCart.objects.create(**validated_data)
-	return cart
+        cart_data = validated_data.pop('owner')
+        cart = DetailCart.objects.create(**validated_data)
+        return cart
 
     class Meta:
         model = DetailCart
@@ -17,13 +19,25 @@ class DetailCartSerializer(serializers.ModelSerializer):
 
 
 class DetailHistorySerializer(serializers.ModelSerializer):
-    #detail_history = serializers.HyperlinkedRelatedField(many=True, view_name='DetailHistory-detail', queryset=DetailHistory.objects.all())
+    activate('America/Chihuahua')
     #id_history = serializers.IntegerField()
     #id_student_fk = serializers.StringRelatedField(queryset=DetailHistory.objects.get(id_student_fk__labs__name='Electronica'))
+    date_out = serializers.DateTimeField(default=datetime.now)
+
     class Meta:
         model = DetailHistory
         fields = ('id_history','id_student_fk', 'id_component_fk', 'quantity', 'date_out', 'date_in')
 
+    def to_representation(self, instance):
+        activate('America/Chihuahua')
+        return {
+            'id_history': instance.id_history,
+            'id_student_fk': instance.id_student_fk.id_student,
+            'id_component_fk': instance.id_component_fk.id_component,
+            'quantity': instance.quantity,
+            'date_out': localtime(instance.date_out),
+            'date_in': instance.date_in
+        }
 
 class ComponentSerializer(serializers.ModelSerializer):
     #component = serializers.HyperlinkedRelatedField(many=True, view_name='Component-detail', queryset=Component.objects.all())
